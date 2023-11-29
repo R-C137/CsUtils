700e960eedc64231be106a665f653373adff9108
Shaun Maurer Masson <inconue1236@gmail.com>
2023-11-29 20:36:59 +0400

    Update HoverSizing.cs
    Added proper namespace

--- Assets/C's Utils/Runtime/UI/HoverSizing.cs
+++ Assets/C's Utils/Runtime/UI/HoverSizing.cs
@@ -11,6 +11,7 @@
  * 
  * Changes: 
  *      [28/11/2023] - Initial implementation (C137)
+ *      [29/11/2023] - Added proper namespace (C137)
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -18,53 +19,56 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HoverSizing : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+namespace CsUtils.UI
 {
-    /// <summary>
-    /// The text on which the hover animation sizing should be done
-    /// </summary>
-    public TMP_Text animatedText;
-
-    /// <summary>
-    /// By how much should the font size be multiplied
-    /// </summary>
-    public float sizeMultiplier = 1.5f;
-
-    /// <summary>
-    /// The original font size of the text before animating
-    /// </summary>
-    float originalFontSize;
-
-    /// <summary>
-    /// Whether auto font sizing was enabled before animating
-    /// </summary>
-    bool autoSizing;
-
-    void Start()
+    public class HoverSizing : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        if(animatedText.gameObject != gameObject)
+        /// <summary>
+        /// The text on which the hover animation sizing should be done
+        /// </summary>
+        public TMP_Text animatedText;
+
+        /// <summary>
+        /// By how much should the font size be multiplied
+        /// </summary>
+        public float sizeMultiplier = 1.5f;
+
+        /// <summary>
+        /// The original font size of the text before animating
+        /// </summary>
+        float originalFontSize;
+
+        /// <summary>
+        /// Whether auto font sizing was enabled before animating
+        /// </summary>
+        bool autoSizing;
+
+        void Start()
         {
-            Debug.LogWarning("The referenced 'animatedText' isn't on the same GameObject as the 'HoverSizing' script. Pointer events will not register properly", gameObject);
+            if (animatedText.gameObject != gameObject)
+            {
+                Debug.LogWarning("The referenced 'animatedText' isn't on the same GameObject as the 'HoverSizing' script. Pointer events will not register properly", gameObject);
+            }
         }
-    }
 
-    private void Reset()
-    {
-        animatedText = animatedText == null ? transform.GetComponent<TMP_Text>() : animatedText;
-    }
+        private void Reset()
+        {
+            animatedText = animatedText == null ? transform.GetComponent<TMP_Text>() : animatedText;
+        }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        animatedText.fontSize = originalFontSize;
-        animatedText.enableAutoSizing = autoSizing;
-    }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            animatedText.fontSize = originalFontSize;
+            animatedText.enableAutoSizing = autoSizing;
+        }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        originalFontSize = animatedText.fontSize;
-        autoSizing = animatedText.enableAutoSizing;
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            originalFontSize = animatedText.fontSize;
+            autoSizing = animatedText.enableAutoSizing;
 
-        animatedText.fontSize *= sizeMultiplier;
-        animatedText.enableAutoSizing = false;
+            animatedText.fontSize *= sizeMultiplier;
+            animatedText.enableAutoSizing = false;
+        }
     }
 }
