3b8528bfb30cf5adfbc8930f713d793eae678aeb
Shaun Maurer Masson <maurermasson.shaun@icloud.com>
2024-05-10 23:16:08 +0400

    Added Tick System Support
    Added a tick system to execute code at a given tick rate

---
+++ Assets/C's Utils/Runtime/Systems/Ticker.cs
@@ -0,0 +1,121 @@
+/* Ticker.cs - C's Utils
+ * 
+ * A tick system useful for optimisation
+ * 
+ * 
+ * Creation Date: 10/05/2024
+ * Authors: C137
+ * Original: C137
+ * 
+ * Edited By: C137
+ * 
+ * Changes: 
+ *      [10/05/2024] - Initial implementation (C137)
+ *      
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CsUtils.Systems.Tick
+{
+    public class Ticker : Singleton<Ticker>
+    {
+        public class Tickable
+        {
+            /// <summary>
+            /// The callback for this tickable
+            /// </summary>
+            public Action callback;
+
+            /// <summary>
+            /// The tick rate at which to execute the callback
+            /// </summary>
+            public int tickRate;
+
+            /// <summary>
+            /// The last tick at which the callback was called
+            /// </summary>
+            public int lastTick;
+
+            public Tickable(Action callback, int tickRate)
+            {
+                this.callback = callback;
+                this.tickRate = tickRate;
+
+                lastTick = 0;
+            }
+        }
+
+        /// <summary>
+        /// How many ticks should be done in one second
+        /// </summary>
+        public int globalTickRate = 60;
+
+        /// <summary>
+        /// The amount of ticks that have elapsed so far
+        /// </summary>
+        public float elapsedTicks;
+
+#pragma warning disable IDE0044 // Add readonly modifier
+        /// <summary>
+        /// Reference to all the tickables subscribed
+        /// </summary>
+        List<Tickable> tickables = new();
+#pragma warning restore IDE0044 // Add readonly modifier
+
+        public void Update()
+        {
+            UpdateTick();
+
+            CallTicks();
+        }
+
+        /// <summary>
+        /// Executes the callbacks for each appropriate tickable
+        /// </summary>
+        void CallTicks()
+        {
+            //If a the ticker has just started or has been reset, no tickable should be called
+            if (Mathf.RoundToInt(elapsedTicks) == 0)
+                return;
+
+            foreach (var tickable in tickables)
+            {
+
+                if (tickable.lastTick != Mathf.RoundToInt(elapsedTicks) && Mathf.RoundToInt(elapsedTicks % tickable.tickRate) == 0)
+                {
+                    tickable.lastTick = Mathf.RoundToInt(elapsedTicks);
+                    tickable.callback();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates the elapsed ticks
+        /// </summary>
+        void UpdateTick()
+        {
+            if (elapsedTicks >= globalTickRate * 10)
+                elapsedTicks = 0;
+
+            elapsedTicks += 60 * Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Subscribe a callback to the tick system
+        /// </summary>
+        /// <param name="tickRate">The tick rate at which the callback should be called<br>Cannot be more than 10x the global tick rate</br></param>
+        /// <param name="callback">The callback to execute at the given tick rate</param>
+        public static void SubscribeTick(int tickRate, Action callback)
+        {
+            singleton.tickables.Add(new(callback, tickRate));
+        }
+
+        public static void UnSubscribeTick(Action callback)
+        {
+            singleton.tickables.RemoveAll((t) => t.callback == callback);
+        }
+    }
+}
